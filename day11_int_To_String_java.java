417587344eb721c7d969b342c923921fcefa67bb
Berlian0204 <100068271+Berlian0204@users.noreply.github.com>
2022-03-02 20:35:04 +0800

    Update day11_int_To_String_java.java

--- day11_int_To_String_java.java
+++ day11_int_To_String_java.java
@@ -1,13 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package berlian;
 
-/**
- *
- * @author user
- */
-public class day11_int_To_String_java {
-    
+    paket  berlian ;
+impor  java.util.* ;
+impor  java.security.* ;
+
+ kelas  publik day11_int_To_String_java {
+    public  static  void  main ( String [] args ) {
+        JanganTerminasi . melarangKeluar();
+
+        coba {
+         Pemindai di =  Pemindai baru  ( Sistem . masuk);
+         int n = di .nextInt();
+         di . menutup();
+         // String s=???; Lengkapi baris di bawah ini
+
+         // Tulis kode Anda di sini
+         // String s = ""+n;
+         // String s = String.nilaiDari(n);
+         String s =  Bilangan bulat . toString(n);
+         // String s = String.format("%d",n);
+         // Bilangan bulat i = n;
+         // StringBuilder sb = new StringBuilder();
+         // sb.tambahkan(i);
+         // String s = sb.toString();
+
+
+
+         if (n ==  Integer . parseInt(s)) {
+          Sistem . keluar . println( " Kerja bagus " );
+         } lain {
+          Sistem . keluar . println( " Jawaban salah. " );
+         }
+        } tangkap ( DoNotTerminate . ExitTrappedException e) {
+         Sistem . keluar . println( " Penghentian Gagal!! " );
+        }   
+    }    
 }
+// Kelas berikut akan mencegah Anda menghentikan kode menggunakan exit(0)!
+kelas  DoNotTerminate {
+
+  kelas statis  publik ExitTrappedException memperluas SecurityException {   
+
+  private  static  final  long serialVersionUID =  1 ;
+}
+
+ public  static  void  forbidExit () {
+  akhir  SecurityManager securityManager =  baru  SecurityManager () {
+   @Mengesampingkan
+   public  void  checkPermission ( Izin  izin ) {
+    if (izin . getName() . berisi( " exitVM " )) {
+     lempar  ExitTrappedException baru  ();
+    }
+   }
+  };
+  Sistem . setSecurityManager(securityManager);
+}
+
